47a32275b09efa0477acae9dc4b8b385f13a2ff4
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-06-08 00:47:40 -0700

    hero 'build hero section'

--- next/src/components/HeroBanner.jsx
+++ next/src/components/HeroBanner.jsx
@@ -21,7 +21,27 @@ const HeroBanner = () => {
         <Image alt='hero' src="/bg-hero5.webp" fill className={`${image === 5 ? "opacity-100" : "opacity-0"} transition-all duration-100`}/>
         <Image alt='hero' src="/bg-hero6.webp" fill className={`${image === 6 ? "opacity-100" : "opacity-0"} transition-all duration-100`}/>
       </div>
-      Hero Banner
+      <div className="z-1 w-[650px] relative flex justify-center flex-col h-full gap-5 ml-20">
+        <h1 className="text-white text-5xl leading-snug">
+          Find the perfect &nbsp; <i>Freelance</i> <br /> services for your business.
+        </h1>
+        <div className="flex align-middle">
+          <div className="relative">
+            <input type="text" className='h-14 w-[450px] pl-10 rounded-md rounded-r-none' placeholder={`Try "building mobile app"`} />
+          </div>
+          <button className="bg-[#1dbf73] text-white px-12 text-lg fontsem rounded-r-md">
+            Search
+          </button>
+        </div>
+        <div className="text-white flex gap-4">
+          Popular: <ul className='flex gap-5'>
+            <li className='text-sm py-1 px-3 border rounded-full hover:bg-white hover:text-black transition-all duration-300 cursor-pointer'>Website design</li>
+            <li className='text-sm py-1 px-3 border rounded-full hover:bg-white hover:text-black transition-all duration-300 cursor-pointer'>Wordpress design</li>
+            <li className='text-sm py-1 px-3 border rounded-full hover:bg-white hover:text-black transition-all duration-300 cursor-pointer'>Logo design</li>
+            <li className='text-sm py-1 px-3 border rounded-full hover:bg-white hover:text-black transition-all duration-300 cursor-pointer'>All services</li>
+          </ul>
+        </div>
+      </div>
     </div>
   )
 }
