11b675f0b82e17bf33524f196ec250e580eb77f0
Lyonsmasawa <lyonsmasawa@gmail.com>
2023-06-12 03:11:56 -0700

    build: 'gigs creation'

--- next/src/pages/seller/gigs/create.jsx
+++ next/src/pages/seller/gigs/create.jsx
@@ -1,9 +1,176 @@
-import React from 'react'
+import { categories } from "@/utils/categories";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import { useCookies } from "react-cookie";
 
 const create = () => {
-  return (
-    <div>create</div>
-  )
-}
+  const [cookies] = useCookies();
+  const router = useRouter();
 
-export default create
+  const [files, setFile] = useState([]);
+  const [features, setFeatures] = useState([]);
+  const [data, setData] = useState({
+    title: "",
+    category: "",
+    description: "",
+    time: 0,
+    revisions: 0,
+    feature: "",
+    price: 0,
+    shortDesc: "",
+  });
+
+  const inputClassName =
+    "block p-4 w-full text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50  focus:ring-blue-500 focus:border-blue-500";
+  const labelClassName = "mb-2 text-lg font-medium text-gray-900 ";
+
+  const handleChange = (e) =>
+    setData({ ...data, [e.target.name]: e.target.value });
+
+  const addFeature = () => {
+    if (data.feature) {
+      setFeatures([...features, data.feature]);
+      setData({ ...data, feature: "" });
+    }
+  };
+
+  const removeFeature = (index) => {
+    const clonedFeatures = [...features];
+    clonedFeatures.splice(index, 1);
+    setFeatures(clonedFeatures);
+  };
+
+  return (
+    <div className="min-h-[80vh] my-10 mt-0 px-32">
+      <h1 className="text-6xl text-gray-900 mb-5">Create a new Gig</h1>
+      <h3 className="text-3xl text-gray-900 mb-5">
+        Enter the details to create the gig
+      </h3>
+      <form action="" className="flex flex-col gap-5 mt-10">
+        <div className="grid grid-cols-2 gap-11">
+          <div>
+            <label htmlFor="title" className={labelClassName}>
+              Gig Title
+            </label>
+            <input
+              name="title"
+              value={data.title}
+              onChange={handleChange}
+              type="text"
+              id="title"
+              className={inputClassName}
+              placeholder="e.g. I will do something I'm really good at"
+              required
+            />
+          </div>
+
+          <div>
+            <label htmlFor="categories" className={labelClassName}>
+              Select a Category
+            </label>
+            <select
+              id="categories"
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-4"
+              name="category"
+              onChange={handleChange}
+              defaultValue="Choose a Category"
+              value={data.category}
+            >
+              {categories.map(({ name }) => (
+                <option key={name} value={name}>
+                  {name}
+                </option>
+              ))}
+            </select>
+          </div>
+        </div>
+        <div>
+          <label htmlFor="description" className={labelClassName}>
+            Gig Description
+          </label>
+          <textarea
+            id="description"
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500"
+            placeholder="Write a short description"
+            name="description"
+            value={data.description}
+            onChange={handleChange}
+          ></textarea>
+        </div>
+        <div className="grid grid-cols-2 gap-11">
+          <div>
+            <label htmlFor="delivery">Delivery Time</label>
+            <input
+              type="number"
+              className={inputClassName}
+              id="delivery"
+              name="time"
+              value={data.time}
+              onChange={handleChange}
+              placeholder="Minimum Delivery Time"
+            />
+          </div>
+          <div>
+            <label htmlFor="revision" className={labelClassName}>
+              Revisions
+            </label>
+            <input
+              type="number"
+              id="revision"
+              className={inputClassName}
+              placeholder="Max Number of Revisions"
+              name="revisions"
+              value={data.revisions}
+              onChange={handleChange}
+            />
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-11">
+          <div>
+            <label htmlFor="features" className={labelClassName}>
+              Features
+            </label>
+            <div className="flex gap-3 items-center mb-5">
+              <input
+                type="text"
+                id="features"
+                className={inputClassName}
+                placeholder="Enter a Feature Name"
+                name="feature"
+                value={data.feature}
+                onChange={handleChange}
+              />
+              <button
+                type="button"
+                className="focus:outline-none text-white bg-blue-700 hover:bg-blue-800  font-medium  text-lg px-10 py-3 rounded-md "
+                onClick={addFeature}
+              >
+                Add
+              </button>
+            </div>
+            <ul className="flex gap-2 flex-wrap">
+              {features.map((feature, index) => {
+                return (
+                  <li
+                    key={feature + index.toString()}
+                    className="flex gap-2 items-center py-2.5 px-5 mr-2 mb-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-red-700 cursor-pointer hover:border-red-200"
+                  >
+                    <span>{feature}</span>
+                    <span
+                      className="text-red-700"
+                      onClick={() => removeFeature(index)}
+                    >
+                      X
+                    </span>
+                  </li>
+                );
+              })}
+            </ul>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default create;
